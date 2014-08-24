b172ba7bd3ad3a8c69d3b68106befc8425c4bdb9
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2014-08-24 12:52:43 +0200

    Using single quotes

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var morgan = require('morgan');
 var env = process.env.NODE_ENV;
 
 var config = require('./config.json')[env];
-console.log(config);
+var port = config.port || 5000;
+var resources = config.resources || '/app/app/';
 
 var cors = function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET POST PUT DELETE PATCH");
-    res.header("Access-Control-Allow-Headers", "Content-Type Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET POST PUT DELETE PATCH');
+    res.header('Access-Control-Allow-Headers', 'Content-Type Accept');
     next();
 }
 var app = express();
@@ -19,6 +20,6 @@ if (env === 'dev') {
 app.use(express.static(__dirname + config.resources));
 
 app.listen(config.port, function () {
-    console.log("Server started on port ", config.port);
+    console.log('Server started on port ', config.port);
 });
 
