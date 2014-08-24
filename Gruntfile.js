41954735e28f9d258edda95029d9cd2ba4f2ee2a
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2014-08-24 15:19:57 +0200

    Setup should be allmost complete

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,109 @@
 /*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Task configuration.
-    jshint: {
-      gruntfile: {
-        src: 'Gruntfile.js'
-      },
-      app: {
-        src: ['app/www/**/*.js']
-      }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      app: {
-        files: '<%= jshint.app.src %>',
-        tasks: ['jshint:app']
-      }
-    }
-  });
+    // Project configuration.
+    var config = require('./config.json');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+    console.log('config', config);
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+    grunt.initConfig({
+        // Task configuration.
+        jshint: {
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            dev: {
+                src: [config.dev.resources + '/**/*.js']
+            }
+        },
+        clean: {
+            prod: {
+                src: [config.prod.resources + '/**/*']
+            },
+            postprod: {
+                src: [config.prod.resources + '/**/*.{js,css}', '!' + config.prod.resources + '/**/optimized.*.{js,css}']
+            }
+        },
+        copy: {
+            prod: {
+                expand: true,
+                cwd: 'app/app/',
+                src: ['**/**.*', '!**/**.scss'],
+                dest: 'app/www/'
+            }
+        },
+        filerev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'sha512',
+                length: 128
+            },
+            prod: {
+                src: config.prod.resources + '**/optimized.{js,css}'
+            }
+        },
+        compass: {
+            dev: {
+                options: {
+                    sassDir: config.dev.resources+'sass',
+                    cssDir: config.dev.resources+'css'
+                }
+            }
+        },
+        useminPrepare: {
+            html: config.prod.resources + 'index.html',
+            options: {
+                dest: config.prod.resources
+            }
+        },
+        usemin: {
+            html: config.prod.resources + 'index.html',
+            options: {
+                blockReplacements: {
+                    js: function (block) {
+                        var name = config.prod.resources+block.dest;
+                        var path = grunt.filerev.summary[name].split('/').splice(2).join('/');
+                        return '<script type="text/javascript" src="' + path + '"></script>';
+                    },
+                    css: function (block) {
+                        var name = config.prod.resources+block.dest;
+                        var path = grunt.filerev.summary[name].split('/').splice(2).join('/');
+                        return '<link rel="stylesheet" type="text/css" href="' + path + '">';
+                    }
+                }
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            js: {
+                files: '<%= jshint.dev.src %>',
+                tasks: ['jshint:dev']
+            },
+            sass: {
+                files: '<%= compass.dev.options.sassDir %>'+'/**/*.scss',
+                tasks: ['compass']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-filerev');
+    grunt.loadNpmTasks('grunt-usemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint:dev', 'compass', 'watch']);
+    grunt.registerTask('dev', ['default']);
+    grunt.registerTask('prod', ['clean:prod', 'copy:prod', 'compass', 'useminPrepare', 'concat:generated', 'cssmin:generated', 'uglify:generated', 'filerev:prod', 'usemin', 'clean:postprod']);
 
 };
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');
-var env = process.env.NODE_ENV;
+var env = process.env.NODE_ENV || 'prod';
 
 var config = require('./config.json')[env];
 var port = config.port || 5000;
 var resources = config.resources || '/app/app/';
+
+console.log('Launching appliction in %s mode', env);
 
 var cors = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -16,8 +18,9 @@ var app = express();
 if (env === 'dev') {
     app.use(morgan('dev'));
     app.use(cors);
+    app.use(express.static(__dirname));
 }
-app.use(express.static(__dirname + config.resources));
+app.use(express.static(__dirname +'/'+ config.resources));
 
 app.listen(config.port, function () {
     console.log('Server started on port ', config.port);
