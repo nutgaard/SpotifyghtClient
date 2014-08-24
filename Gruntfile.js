060c1481e32b0bfd44454c42d0bbeb948109d2c6
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2014-08-24 12:50:10 +0200

    Setup of foreman build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,44 +5,26 @@ module.exports = function(grunt) {
   grunt.initConfig({
     // Task configuration.
     jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        globals: {}
-      },
       gruntfile: {
         src: 'Gruntfile.js'
       },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
+      app: {
+        src: ['app/www/**/*.js']
       }
-    },
-    nodeunit: {
-      files: ['test/**/*_test.js']
     },
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
         tasks: ['jshint:gruntfile']
       },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
+      app: {
+        files: '<%= jshint.app.src %>',
+        tasks: ['jshint:app']
       }
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 var express = require('express');
-var config = {port: process.env.PORT || 5000};
+var morgan = require('morgan');
+var env = process.env.NODE_ENV;
 
+var config = require('./config.json')[env];
+console.log(config);
 
-var cors = function(req, res, next){
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Methods", "GET POST PUT DELETE PATCH");
-	res.header("Access-Control-Allow-Headers", "Content-Type Accept");
-	next();
+var cors = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET POST PUT DELETE PATCH");
+    res.header("Access-Control-Allow-Headers", "Content-Type Accept");
+    next();
 }
 var app = express();
+if (env === 'dev') {
+    app.use(morgan('dev'));
+    app.use(cors);
+}
+app.use(express.static(__dirname + config.resources));
 
-app.use(express.static(__dirname + "/app/www/"));
-
-app.listen(config.port, function(){
-	console.log("Server started on port ", config.port);
+app.listen(config.port, function () {
+    console.log("Server started on port ", config.port);
 });
 
