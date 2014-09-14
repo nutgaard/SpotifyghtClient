98e025ba1f4f73657b531432ea90addc6935aafc
Nicklas Utgaard <nutgaard@gmail.com>
2014-09-14 22:54:09 +0200

    More styling

--- Gruntfile.js
+++ Gruntfile.js
@@ -92,7 +92,7 @@ module.exports = function (grunt) {
                 tasks: ['jshint:dev']
             },
             sass: {
-                files: config.dev.resources + '/**/*.scss',
+                files: config.dev.resources + '**/*.scss',
                 tasks: ['compass']
             }
         }
