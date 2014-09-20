bfb454252c5ba088633be68ea07e0a7c4084a84c
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2014-09-20 15:58:14 +0200

    Started on the connection between the web and actual spotify client

--- app/app/components/spotifyplayer/SpotifyPlayerDirective.js
+++ app/app/components/spotifyplayer/SpotifyPlayerDirective.js
@@ -9,12 +9,76 @@
             };
         }])
         .controller('SpotifyPlayerCtrl', ['$scope', function ($scope) {
+            $scope.player = undefined;
+            $scope.status = undefined;
+            $scope.playerstate = false;
             $scope.current = {
-                song: 'Tore tang',
-                artist: 'Mods',
-                album: 'Et eller annet',
+                song: '',
+                artist: '',
+                album: '',
+                position: 0,
+                songlength: 100,
                 uri: ''
             };
-            $scope.isplaying = function(){return true;};
+            $scope.intervalUpdate = undefined;
+
+            Spotify.start('bf1a2d09e98ce4c077b70f965bd12faa').done(function (ok, instance, status) {
+                $scope.$apply(function () {
+                    $scope.player = instance;
+                    $scope.status = status;
+                    $scope.startLongPolling();
+                });
+            }).fail(function () {
+                console.log('Could not connect to spotify client');
+            });
+
+            $scope.$watch('status', function (status) {
+                if (typeof status === 'undefined') {
+                    return;
+                }
+                console.log('status update: ', status);
+                $scope.current.song = status.track.track_resource.name;
+                $scope.current.artist = status.track.artist_resource.name;
+                $scope.current.album = status.track.album_resource.name;
+                $scope.current.uri = status.track.track_resource.uri;
+                $scope.current.position = status.playing_position;
+                $scope.current.songlength = status.track.length;
+                $scope.playerstate = status.playing;
+                updateProgessPercent();
+            });
+            $scope.$watch('playerstate', function (state) {
+                if (state) {
+                    $scope.intervalUpdate = setInterval(function () {
+                        $scope.current.position += 0.25;
+                        updateProgessPercent();
+                    }, 250);
+                } else if ($scope.intervalUpdate > 0) {
+                    clearInterval($scope.intervalUpdate);
+                }
+
+            });
+            $scope.startLongPolling = function () {
+                $scope.player.status().then(function (status) {
+                    $scope.$apply(function () {
+                        $scope.status = status;
+                    });
+                    $scope.startLongPolling();
+                });
+            };
+            $scope.isplaying = function () {
+                return $scope.playerstate;
+            };
+            $scope.playpause = function () {
+                $scope.playerstate = !$scope.playerstate;
+            };
+
+            function playerReady() {
+                return typeof $scope.player !== 'undefined';
+            }
+
+            function updateProgessPercent() {
+                var p = ($scope.current.position / $scope.current.songlength) * 100;
+                $('.playbarprogress').css({width: p + '%'});
+            }
         }]);
 })();
