40b5fad668dafef31d16818babd3f0def53abdba
Eivind Larsen <eivinlar@stud.ntnu.no>
2014-10-17 19:23:46 +0200

    more nginx proxy helping

--- app/app/js/services/SocketService.js
+++ app/app/js/services/SocketService.js
@@ -1,12 +1,16 @@
 (function () {
     'use strict';
+    var striptrailslash = function (site) {     
+      return site.replace(/\/$/, "");
+    } 
 
     var SocketService = angular.module('SocketService', ['btford.socket-io']);
 
     SocketService.factory('socket', ['$rootScope', 'socketFactory', 'API_ENDPOINT',
         function ($rootScope, socketFactory, API_ENDPOINT) {
 //            var ioSocket = io.connect('ws://localhost:3001/group/eivind');
-            var ioSocket = io.connect(API_ENDPOINT);
+	    var url = striptrailslash(API_ENDPOINT);
+            var ioSocket = io.connect(url, {'resource': 'api/socket.io' } );
 
             var mySocket = socketFactory( {
                 ioSocket: ioSocket
